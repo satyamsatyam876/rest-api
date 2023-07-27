65b8c71ace66e9c663daec1c5e454936286b65f9
Satyam <satyam.07srivastava2000@gmail.com>
2023-07-27 11:12:53 +0530

    Update operation done

--- routes/dev.js
+++ routes/dev.js
@@ -42,7 +42,19 @@ catch(err){
 
 // put/patch update a developer
 
+router.patch("/update/:id", async(req,res)=>{
+try{
+const{id:_id}= await req.params;
+const changes=await req.body
+const updatedDeveloper=await Developer.findByIdAndUpdate(_id,{...changes,_id})
+res.status(200).json({message:"Sucess"})
 
+}
+catch(err){
+    res.status(400).json({message:"Error"}) 
+ }
+
+})
 
 
 
